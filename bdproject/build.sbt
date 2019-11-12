8021ef10f44a175385633a7eefc82b5e62d6f3c5
Abdurasul Rakhimov <rakhimovabdurasul@gmail.com>
2019-11-12 23:25:23 +0300

    Implemented reading input and writing output

--- bdproject/build.sbt
+++ bdproject/build.sbt
@@ -8,5 +8,7 @@ libraryDependencies ++= Seq(
   "org.apache.spark" %% "spark-core" % "2.4.4",
   "org.apache.spark" %% "spark-sql" % "2.4.4",
   "org.apache.spark" %% "spark-mllib" % "2.4.4",
-  "log4j" % "log4j" % "1.2.17"
+  "log4j" % "log4j" % "1.2.17",
+  "com.databricks" %% "spark-avro" % "3.2.0",
+  "com.google.guava" % "guava" % "15.0"
 )
--- bdproject/src/main/scala/Model.scala
+++ bdproject/src/main/scala/Model.scala
@@ -3,33 +3,42 @@ import org.apache.spark.ml.classification.LogisticRegression
 import org.apache.spark.ml.classification.DecisionTreeClassifier
 import org.apache.spark.ml.feature.{HashingTF, Tokenizer}
 import org.apache.spark.ml.linalg.Vector
-import org.apache.spark.sql.Row
-// $example off$
+import org.apache.spark.sql.{Column, Row}
+import org.apache.spark.sql.types.{IntegerType, StringType, StructField, StructType}
 import org.apache.spark.sql.SparkSession
 import org.apache.log4j.BasicConfigurator
+import org.apache.spark.sql.functions._
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkConf
 import org.apache.log4j.Logger
 
 object Model {
 
   def main(args: Array[String]): Unit = {
+    val testOutputPath = "/home/abdurasul/output"
+    val masterNodeAddress = "spark://10.91.54.103:7077"
+    val modelSavePath = "/home/abdurasul/decision-tree"
 
     BasicConfigurator.configure()
     val spark = SparkSession
       .builder()
-      .appName("PipelineExample")
-      .master("spark://10.91.54.103:7077")
+      .appName("Twitter Sentiment Analysis")
+      .master(masterNodeAddress)
       .getOrCreate()
 
-    // $example on$
-    // Prepare training documents from a list of (id, text, label) tuples.
-    val training = spark.createDataFrame(Seq(
-      (0L, "a b c d e spark", 1.0),
-      (1L, "b d", 0.0),
-      (2L, "spark f g h", 1.0),
-      (3L, "hadoop mapreduce", 0.0)
-    )).toDF("id", "text", "label")
+    val customSchema = StructType(Array(
+      StructField("id", IntegerType, true),
+      StructField("label", IntegerType, true),
+      StructField("text", StringType, true))
+    )
 
-    // Configure an ML pipeline, which consists of three stages: tokenizer, hashingTF, and lr.
+    val customTestSchema = StructType(Array(
+      StructField("id", IntegerType, true),
+      StructField("text", StringType, true)
+    ))
+
+    val training = spark.read.format("csv").option("header", "true").schema(customSchema).load("dataset/train.csv")
+
     val tokenizer = new Tokenizer()
       .setInputCol("text")
       .setOutputCol("words")
@@ -46,35 +55,23 @@ object Model {
     val pipeline = new Pipeline()
       .setStages(Array(tokenizer, hashingTF, dt))
 
-    // Fit the pipeline to training documents.
     val model = pipeline.fit(training)
 
-    // Now we can optionally save the fitted pipeline to disk
-    model.write.overwrite().save("/home/abdurasul/decision-tree")
 
-    // We can also save this unfit pipeline to disk
-    pipeline.write.overwrite().save("/tmp/unfit-lr-model")
+    model.write.overwrite().save(modelSavePath)
 
-    // And load it back in during production
-//    val sameModel = PipelineModel.load("/home/abdurasul/decision-tree")
+    val test = spark.read.format("csv").option("header", "true").schema(customTestSchema).load("dataset/test.csv")
 
-    // Prepare test documents, which are unlabeled (id, text) tuples.
-    val test = spark.createDataFrame(Seq(
-      (4L, "spark i j k"),
-      (5L, "l m n"),
-      (6L, "spark hadoop spark"),
-      (7L, "apache hadoop")
-    )).toDF("id", "text")
+    import spark.implicits._
+    val tested = model.transform(test)
+      .select("id", "text", "prediction")
 
-    // Make predictions on test documents.
-    model.transform(test)
-      .select("id", "text", "probability", "prediction")
-      .collect()
-      .foreach { case Row(id: Long, text: String, prob: Vector, prediction: Double) =>
-        println(s"($id, $text) --> prob=$prob, prediction=$prediction")
-      }
-    // $example off$
+
+    tested.repartition(10).write.csv(testOutputPath)
 
     spark.stop()
   }
+
+  //  def stringify(c: Column) = concat(lit("["), concat_ws(",", c), lit("]"))
+
 }
